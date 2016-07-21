df65e633ff93bccf97d26abf99ac3589c52fda41
marck1391 <marck1391@gmail.com>
2016-07-21 06:09:49 -0500

    Views/Templates

--- polls/views.py
+++ polls/views.py
@@ -1,8 +1,20 @@
-from django.shortcuts import render
-
-# Create your views here.
-
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, JsonResponse
+from .models import Question
 
 def index(request):
-	return HttpResponse("Hello world")
+	questions = Question.objects.order_by('-pub_date')[:5]
+	data = {
+		'question_list': questions
+	}
+	return render(request, 'polls/index.html', data)
+
+def detail(request, question_id):
+	question = get_object_or_404(Question, id=question_id)
+	return render(request, 'polls/detail.html', {'question': question})
+
+def results(request, question_id):
+	return HttpResponse("Question results: %s"%question_id)
+
+def vote(request, question_id):
+	return JsonResponse({'test':'json'})
